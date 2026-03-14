5325f315edda4f059b7493546d43e2277e72f23e
Alvaro499 <alvarosiles499@gmail.com>
2026-03-14 17:57:14 -0600

    Register Form - Top Menu (Register User)

--- Blog/apps/user/views.py
+++ Blog/apps/user/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from apps.user.forms import SignUpForm
 
+class SignUpView(CreateView):
+
+    form_class = SignUpForm
+    template_name = 'login/register.html'
+    success_url = reverse_lazy('index')
 
 def login(request):
-    return HttpResponse("Hello World")
-
-
-def register(request):
     return HttpResponse("Hello World")
