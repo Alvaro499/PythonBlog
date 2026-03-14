b5ae4b88901b294c8308a976fdc382416db659aa
Alvaro499 <alvarosiles499@gmail.com>
2026-03-13 22:40:12 -0600

    New User Entity - ORM

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -77,10 +77,17 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog_database',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
+
+#do not use auth_user by default, instead use User from app "user"
+AUTH_USER_MODEL = 'user.User'
 
 
 # Password validation
--- Blog/apps/user/models.py
+++ Blog/apps/user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Allow us to extract the EntityUser by default created at the begining
+from django.contrib.auth.models import AbstractUser
+
+# We are creating our models for the database using two aprox:
+    # Inhereting the entities and attributes created before with commands:
+        # pip install psycopg2
+        # py managae.py migrate
+
+    # Manually creation, similar as full_name
+class User(AbstractUser):
+    full_name = models.CharField(max_length=255)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    photo = models.ImageField(blank=True, null=True)
+
